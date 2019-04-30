6cbca93b0d36a2bda5791295f56cd0a8354ef5d5
Your Name <you@example.com>
2019-04-30 11:27:27 +0800

    整合路由及首页

--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import './assets/statics/site/css/style.css'
+import './assets/statics/site/css/style.css'  //导入全局样式
+
+import VueRouter from 'vue-router'  //导入路由
+import index from './components/index.vue'  //导入组件
+Vue.use(VueRouter)
+const routes =[   //写路由规则
+  {path:'/index',component:index}
+]   
+const router = new VueRouter({  //实例化路由对象
+  routes
+})
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router   //挂到顶级Vue实例上
 }).$mount('#app')
